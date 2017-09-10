8bc73454cc7aa455bf1f71609ee3c9fc1c327639
Elliot Ali <elliot.ali@me.com>
2017-09-10 17:35:55 +0100

    ajax added

--- app/Fishtank.php
+++ app/Fishtank.php
@@ -9,21 +9,37 @@ class Fishtank
     public static function set($state)
     {
         $file = "mode.txt";
-        Storage::disk('fishtank')->put($file, $state . "\n");
 
-        switch($state)
+        if( $state == 'auto' )
         {
-            case 'day':
-                break;
-
-            case 'night':
-                break;
-
-            case 'off':
-                break;
-            default:
-            //auto
+            if( self::daylight() )
+            {
+                Storage::disk('fishtank')->put($file, "day\n");
+            } else {
+                Storage::disk('fishtank')->put($file, "night\n");
+            }
+        } else {
+            Storage::disk('fishtank')->put($file, $state . "\n");
         }
+    }
+
+    public static function daylight()
+    {
+        $latitude = config('fishtank.location.latitude');
+        $longitude = config('fishtank.location.longitude');
+
+        $sunrise = date_sunrise(time(), SUNFUNCS_RET_STRING, $latitude, $longitude);
+        $sunrise = date_sunset(time(), SUNFUNCS_RET_STRING, $latitude, $longitude);
+
+        return ( time() > $sunrise && time() < $sunset );
+    }
+
+    public static function data()
+    {
+        return [
+            'time' => time(),
+            'timezone' => config('app.timezone'),
+        ];
     }
 
 }
---
+++ config/fishtank.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fishtank Settings
+    |--------------------------------------------------------------------------
+    |
+    | This file allows you to set the parameters for your fishtank e.g. location
+    |
+    */
+
+    'location' => [
+        'longitude' => env('LOCATION_LONG', '-1.0923960'),
+        'latitude' => env('LOCATION_LAT', '51.2665400'),
+    ],
+
+];
